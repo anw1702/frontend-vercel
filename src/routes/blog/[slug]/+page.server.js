aec2c5bf478ff12105f48de2fcfaded05301ad11
Anwesha Nema <anweshanema17@gmail.com>
2024-07-06 23:16:07 +0530

    added vercel-postgres

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -3,3 +3,11 @@ export function load({params}) {
    content :`helloworld ${params.slug}`
 }
 }
+
+import { sql } from "@vercel/postgres";
+
+export async function load({params, locals }) {
+  return {
+    names: await sql`SELECT * from NAMES where user_id='${locals.user}'`
+  }
+}
